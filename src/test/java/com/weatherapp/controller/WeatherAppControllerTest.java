a4bfc98483ddc2ce082fa7e0ded85e2c6d9cdcbc
“Krishna” <iamkrishv@gmail.com>
2019-02-10 17:08:52 +0100

    Jacoco plugin has been added in the pom.xml and minor changes in the controller test file

--- src/test/java/com/weatherapp/controller/WeatherAppControllerTest.java
+++ src/test/java/com/weatherapp/controller/WeatherAppControllerTest.java
@@ -11,16 +11,25 @@ import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.weatherapp.service.WeatherAppServiceImpl;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebMvcTest(WeatherAppController.class)
 public class WeatherAppControllerTest {
 
+	
+	@MockBean
+	public WeatherAppServiceImpl weatherAppServiceImplMock;
+	
+	
 	@InjectMocks
 	private WeatherAppController weatherAppController;
+	
 	
 	@Autowired
 	MockMvc mockMvc;
